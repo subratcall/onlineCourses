2e706c8e0cc7b50034e6c803762de1c57a91a942
bram <ibrahim.bramantyo@gmail.com>
2020-08-06 10:26:03 +0700

    dash updated

--- dash/section-03E-C.py
+++ dash/section-03E-C.py
@@ -1,1 +1,9 @@
 import plotly.offline as pyo 
+import plotly.graph_objs as go 
+import pandas as pd 
+
+df = pd.read_csv('Data/mpg.csv', index_col=0)
+print(df.head())
+data = [go.Scatter(
+    x = df['horsepower']
+)]
